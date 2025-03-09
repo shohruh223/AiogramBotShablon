390331202a275124594475d0f92c5eecec20cde9
Nigmatullayev001 <salohiddinnugmatullaev@gmail.com>
2025-03-09 14:52:16 +0500

    commit v3

--- handler/__init__.py
+++ handler/__init__.py
@@ -1,5 +1,5 @@
 from aiogram import Router
-from .users import start, reply_keyboard_handler, inline_keyboard_handler
+from .users import start, inline_keyboard_handler
 
 
 def setup_message_routers() -> Router:
@@ -7,7 +7,6 @@ def setup_message_routers() -> Router:
 
     # Users routers
     router.include_router(start.router)
-    router.include_router(reply_keyboard_handler.router)
     router.include_router(inline_keyboard_handler.router)
 
     return router
