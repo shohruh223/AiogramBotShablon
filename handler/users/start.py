d6984f2a68c14c8ffe43de11e4c43d8738187dbe
Nigmatullayev001 <salohiddinnugmatullaev@gmail.com>
2025-03-11 11:36:58 +0500

    commit v3

--- handler/users/start.py
+++ handler/users/start.py
@@ -2,9 +2,6 @@ from aiogram import Router, F
 from aiogram import types
 from aiogram.filters import Command
 
-from keyboard.default.main_keyboard import main_menu, reply_keyboards
-from keyboard.inline.test import inline_main_menu
-
 router = Router()
 
 
@@ -12,14 +9,4 @@ router = Router()
 async def send_start(message: types.Message):
     await message.answer("Salom bu Shohruh Abdurahmon tomonidan yaratilgan bot shablon!\n"
                          "Mualliflik huquqi bor shu sababli foydalanishdan oldin\n"
-                         "meni duo qilib ishlating, aks holda rozi emasman!", reply_markup=reply_keyboards())
-
-
-@router.message(F.text == "Reply Keyboard")
-async def send_help(message: types.Message):
-    await message.answer("Bu Replay Keyboard'lar", reply_markup=main_menu())
-
-
-@router.message(F.text == "Inline Keyboard")
-async def send_help(message: types.Message):
-    await message.answer("Bu Inline Keyboard'lar", reply_markup=inline_main_menu())
+                         "meni duo qilib ishlating, aks holda rozi emasman!")
--- keyboard/default/main_keyboard.py
+++ keyboard/default/main_keyboard.py
@@ -1,32 +1,19 @@
-from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 
-def main_menu():
-    button = KeyboardButton(text="Reply Keyboard 1")
-    button2 = KeyboardButton(text="Reply Keyboard 2")
-    button3 = KeyboardButton(text="Lakatsiya ulashish", request_location=True)
-    button4 = KeyboardButton(text="Telefon raqamni ulashish", request_contact=True)
-    button5 = KeyboardButton(text="Ortga")
-
+def contact_menu():
+    button = KeyboardButton(text="Contact", request_contact=True)
     rkm = ReplyKeyboardMarkup(
         keyboard=[
-            [button, button2],
-            [button3, button4],
-            [button5]
-        ],
-        resize_keyboard=True
-    )
+            [button]],
+        resize_keyboard=True)
     return rkm
 
 
-def reply_keyboards():
-    button = KeyboardButton(text="Reply Keyboard")
-    button1 = KeyboardButton(text="Inline Keyboard")
+def location_menu():
+    button = KeyboardButton(text="Location", request_location=True)
     rkm = ReplyKeyboardMarkup(
         keyboard=[
-            [button],
-            [button1],
-        ],
-        resize_keyboard=True
-    )
+            [button]],
+        resize_keyboard=True)
     return rkm
